a869f2ce7ba562e7151ad1ebd305c56148a273eb
Lawton Nichols <lawtonnichols@gmail.com>
2023-03-16 13:20:58 -0700

    .

---
+++ csci40-online/lec19/getLineAndAppend.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main() {
+  ofstream ofs; 
+  ofs.open("out.txt", ios_base::app);
+
+  // equivalent to:
+  // ofstream ofs("out.txt", ios_base::app)
+
+  while (true) {
+    string line;
+    cout << "Enter the next line: ";
+    getline(cin, line);
+
+    if (line == "q") {
+      break;
+    }
+
+    // add this line to the file
+    ofs << line << endl;
+  }  
+
+
+  ofs.close();
+
+  return 0;
+}
---
+++ csci40-online/lec19/ostreams.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+void write1to10(ostream& os) {
+  for (int i = 1; i <= 10; i++) {
+    os << i << endl;
+  }
+}
+
+int main() {
+  ofstream ofs("1to10.txt"); 
+
+  write1to10(ofs);
+
+  write1to10(cout);
+
+  return 0;
+}
