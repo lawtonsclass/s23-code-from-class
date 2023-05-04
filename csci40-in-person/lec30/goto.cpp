ecfba8429a4aee2de95bb48647aa79249a80311a
Lawton Nichols <lawtonnichols@gmail.com>
2023-05-04 16:29:19 -0700

    .

---
+++ csci40-in-person/lec30/goto.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int i = 1;
+loopstart:
+  if (i > 10) {
+    goto done;
+  }
+
+  cout << i << endl;
+  i++;
+  goto loopstart;
+
+done:
+
+  return 0;
+}
